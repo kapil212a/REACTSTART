dfe523a37f0da56c61d2dea5a31c325d2ff2e465
kapil212a <amangupta74086@gmail.com>
2026-07-06 01:09:45 +0530

    add

---
+++ newReact/src/Activity/LikeButton.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export default function LikeButton(){
+    let [isLike , setIsLiked] = useState(false);
+    let [click, setClick] = useState(0);
+
+    let clicked = () =>{
+        setIsLiked(!isLike);
+        setClick(click + 1);
+    }
+
+    let st = {color : "red"};
+    return(
+        <div>
+            <p>count = {click}</p>
+            <p onClick={clicked}>{isLike ? <i className ="fa-solid fa-heart" style={st} ></i> : <i className ="fa-regular fa-heart"></i>}</p>
+        </div>
+    )
+}
--- newReact/src/App.jsx
+++ newReact/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 import Counter from "./Counter.jsx";
+import LikeButton from "./Activity/LikeButton.jsx";
 
 function App() {
   return(
     <>
-      <h1>States in React</h1>
-      <Counter/>
+      {/* <h1>States in React</h1>
+      <Counter/> */}
+      <LikeButton/>
+
     </>
   )
 }
