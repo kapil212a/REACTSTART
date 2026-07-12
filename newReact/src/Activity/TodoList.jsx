4bf23ee238b589a2b84bb507f3b74617fe41c78c
kapil212a <amangupta74086@gmail.com>
2026-07-12 22:39:30 +0530

    add

---
+++ newReact/src/Activity/TodoList.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+export default function TodoList(){
+    let [todos , setTodo] = useState(["sample"]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        //console.log("new task added");
+        setTodo([...todos , newTodo]);
+    }
+
+    let updateTodoValue = (event) => {
+        //console.log(event.target);
+        setNewTodo(event.target.value);
+    }
+    return(
+        <div>
+            <input placeholder = "Add Task" value={newTodo} onChange={updateTodoValue} ></input>
+            <button onClick={addNewTask}>Add Task</button>
+            <br></br><br></br><br></br>
+
+            <hr></hr>
+            <h1>This Is My To-Do List</h1>
+            <ul>
+                {todos.map((todo) =>(
+                    <li>{todo}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
