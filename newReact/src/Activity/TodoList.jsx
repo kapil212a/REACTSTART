658a5e6e5466d3ca93dc00c13a08bf7f6fc852a7
kapil212a <amangupta74086@gmail.com>
2026-07-14 18:56:51 +0530

    add

--- newReact/src/Activity/TodoList.jsx
+++ newReact/src/Activity/TodoList.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
 export default function TodoList(){
-    let [todos , setTodo] = useState(["sample"]);
+    //let [todos , setTodo] = useState(["sample"]);
+    let [todos, setTodo] = useState([{task : "sample" , id : uuidv4()}])
     let [newTodo, setNewTodo] = useState("");
 
     let addNewTask = () => {
         //console.log("new task added");
-        setTodo([...todos , newTodo]);
+        //setTodo([...todos , newTodo]);
+        setTodo([...todos , {task : newTodo , id : uuidv4()}])
         setNewTodo("");
     }
 
@@ -23,8 +27,11 @@ export default function TodoList(){
             <hr></hr>
             <h1>This Is My To-Do List</h1>
             <ul>
+                {/* {todos.map((todo) =>(
+                    <li >{todo}</li>
+                ))} */}
                 {todos.map((todo) =>(
-                    <li>{todo}</li>
+                    <li key = {todo.id}>{todo.task}</li>
                 ))}
             </ul>
         </div>
